61cd6d87e860ac242b699944229dabf9087b54e1
Fabrizio <Fabrizio.Ferro@ge.infn.it>
2017-12-07 16:22:14 +0100

    cleaning

--- SimCTPPS/CTPPSPixelDigiProducer/src/CTPPSPixelDigiProducer.cc
+++ SimCTPPS/CTPPSPixelDigiProducer/src/CTPPSPixelDigiProducer.cc
@@ -1,4 +1,3 @@
-// #include "SimTotem/RPDigiProducer/interface/RPDigiProducer.h"
 #include "SimCTPPS/CTPPSPixelDigiProducer/interface/CTPPSPixelDigiProducer.h"
 
 // user include files
@@ -19,12 +18,7 @@
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 
-//#include "DataFormats/TotemRPDataTypes/interface/RPStripDigi.h"
-//#include "DataFormats/TotemRPDataTypes/interface/RPDetTrigger.h"
-
 #include "DataFormats/Common/interface/DetSetVector.h"
-//#include "DataFormats/TotemRPDetId/interface/TotRPDetId.h"
-//#include "DataFormats/CTPPSDetId/interface/CTPPSPixelDetId.h"
 
 //Random Number
 #include "FWCore/ServiceRegistry/interface/Service.h"
@@ -32,43 +26,23 @@
 #include "FWCore/Utilities/interface/Exception.h"
 #include "CLHEP/Random/RandomEngine.h"
 
-//#include "TotemCondFormats/DAQInformation/interface/AnalysisMask.h"                           // needed??????????
-//#include "TotemCondFormats/DataRecord/interface/TotemDAQMappingRecord.h"              // needed??????????
 
 CTPPSPixelDigiProducer::CTPPSPixelDigiProducer(const edm::ParameterSet& conf) :
-	conf_(conf) {
-	//register your products
-	/* Examples
-	 produces<ExampleData2>();
+  conf_(conf) {
 
-	 //if do put with a label
-	 produces<ExampleData2>("label");
-	 */
-	//now do what ever other initialization is needed
-	//  std::string strip_digi_label ( conf.getParameter<std::string>("StripDigiLabel") );
+  produces<edm::DetSetVector<CTPPSPixelDigi> > ();
 
-	produces<edm::DetSetVector<CTPPSPixelDigi> > ();
-//	produces<edm::DetSetVector<RPDetTrigger> > ();
+// register data to consume
+  tokenCrossingFrameCTPPSPixel = consumes<CrossingFrame<PSimHit>>(edm::InputTag("mix","g4SimHitsCTPPSPixelHits"));
 
-	// register data to consume
-	tokenCrossingFrameCTPPSPixel = consumes<CrossingFrame<PSimHit>>(edm::InputTag("mix","g4SimHitsCTPPSPixelHits"));
+  RPix_hit_containers_.clear();
+  RPix_hit_containers_ = conf.getParameter<std::vector<std::string> > ("ROUList");
+  verbosity_ = conf.getParameter<int> ("RPixVerbosity");
 
-	RPix_hit_containers_.clear();
-	RPix_hit_containers_ = conf.getParameter<std::vector<std::string> > ("ROUList");
-	verbosity_ = conf.getParameter<int> ("RPixVerbosity");
-
-//	simulateDeadChannels = false;
-//	if (conf.exists("simulateDeadChannels")) { //check if "simulateDeadChannels" variable is defined in configuration file
-//		simulateDeadChannels = conf.getParameter<bool> ("simulateDeadChannels");
-//	}
-}
-
-CTPPSPixelDigiProducer::~CTPPSPixelDigiProducer() {
-
-	// do anything here that needs to be done at desctruction time
-	// (e.g. close files, deallocate resources etc.)
 
 }
+
+CTPPSPixelDigiProducer::~CTPPSPixelDigiProducer() {}
 
 //
 // member functions
@@ -76,170 +50,102 @@ CTPPSPixelDigiProducer::~CTPPSPixelDigiProducer() {
 
 // ------------ method called to produce the data  ------------
 void CTPPSPixelDigiProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
-	using namespace edm;
-	/* This is an event example
-	 //Read 'PSimHit' from the Event
-	 Handle<PSimHit> pIn;
-	 iEvent.getByLabel("TotemHitsRP",pIn);
+  using namespace edm;
+// initialize random engine
+  if(!rndEngine) {
+    Service<RandomNumberGenerator> rng;
+    if (!rng.isAvailable()) {
+      throw cms::Exception("Configuration") << "This class requires the RandomNumberGeneratorService\n"
+	"which is not present in the configuration file.  You must add the service\n"
+	"in the configuration file or remove the modules that require it.";
+    }
+    rndEngine = &(rng->getEngine(iEvent.streamID()));
+  }
 
-	 //Use the ExampleData to create an ExampleData2 which
-	 // is put into the Event
-	 std::auto_ptr<ExampleData2> pOut(new ExampleData2(*pIn));
-	 iEvent.put(pOut);
-	 */
+// Step A: Get Inputs
+  edm::Handle<CrossingFrame<PSimHit> > cf;
 
-	/* this is an EventSetup example
-	 //Read SetupData from the SetupRecord in the EventSetup
-	 ESHandle<SetupData> pSetup;
-	 iSetup.get<SetupRecord>().get(pSetup);
-	 */
+  iEvent.getByToken(tokenCrossingFrameCTPPSPixel, cf);
 
-	// initialize random engine
-	if(!rndEngine) {
-		Service<RandomNumberGenerator> rng;
-		if (!rng.isAvailable()) {
-			throw cms::Exception("Configuration") << "This class requires the RandomNumberGeneratorService\n"
-					"which is not present in the configuration file.  You must add the service\n"
-					"in the configuration file or remove the modules that require it.";
-		}
-		rndEngine = &(rng->getEngine(iEvent.streamID()));
-	}
+  if (verbosity_) {
+    std::cout << "\n\n=================== Starting SimHit access" << "  ===================" << std::endl;
 
-	// Step A: Get Inputs
-	edm::Handle<CrossingFrame<PSimHit> > cf;
-//	iEvent.getByToken(tokenCrossingFrameTotemRP, cf);
-	iEvent.getByToken(tokenCrossingFrameCTPPSPixel, cf);
-//	iEvent.getByLabel("mix", "g4SimHitsCTPPSPixelHits", cf);
-
-	if (verbosity_) {
-		std::cout << "\n\n=================== Starting SimHit access" << "  ===================" << std::endl;
-
-		std::auto_ptr<MixCollection<PSimHit> > col(
-		        new MixCollection<PSimHit> (cf.product(), std::pair<int, int>(-0, 0)));
-		std::cout << *(col.get()) << std::endl;
-		MixCollection<PSimHit>::iterator cfi;
-		int count = 0;
-		for (cfi = col->begin(); cfi != col->end(); cfi++) {
-			std::cout << " Hit " << count << " has tof " << cfi->timeOfFlight() << " trackid "
-			        << cfi->trackId() << " bunchcr " << cfi.bunch() << " trigger " << cfi.getTrigger()
-			        << ", from EncodedEventId: " << cfi->eventId().bunchCrossing() << " "
-			        << cfi->eventId().event() << " bcr from MixCol " << cfi.bunch() << std::endl;
-			std::cout << " Hit: " << (*cfi) << "  " << cfi->exitPoint()<<std::endl;
-			count++;
-		}
-	}
-
-	/*    if( verbosity_ ){
-	 std::pair<int,int> range=cf->getBunchRange();
-	 std::cout << "Nr of pileups " << cf->getNrPileups() << std::endl;
-	 std::vector<PSimHit>::const_iterator it_begin;
-	 std::vector<PSimHit>::const_iterator it_end;
-	 cf->getPileups(it_begin,it_end);
-	 std::vector<PSimHit>::iterator it;
-	 for( std::vector<PSimHit>::const_iterator it = it_begin; it != it_end ; it++ )
-	 std::cout<<" PSimHit "<<(*it)<<std::endl;
-	 std::cout << "Crossing frame id for subdet "<<cf->getEventID()<<",  bunchrange = "<<range.first<<","<<range.second  <<", bunchSpace "<<cf->getBunchSpace() << std::endl;
-	 } */
-
-	//std::auto_ptr<MixCollection<PSimHit> > allRPHits(new MixCollection<PSimHit>(cf.product(),RP_hit_containers_));
-	std::auto_ptr<MixCollection<PSimHit> > allRPixHits(
-	        new MixCollection<PSimHit> (cf.product(), std::pair<int, int>(0, 0)));
-
-	if (verbosity_)
-		std::cout << "Input MixCollection size = " << allRPixHits->size() << std::endl;
-
-	//Loop on PSimHit
-	SimHitMap.clear();
-
-	MixCollection<PSimHit>::iterator isim;
-	for (isim = allRPixHits->begin(); isim != allRPixHits->end(); ++isim) {
-		SimHitMap[(*isim).detUnitId()].push_back((*isim));
-	}
-
-	// Step B: LOOP on hits in event
-	theDigiVector.reserve(400);
-	theDigiVector.clear();
+    std::auto_ptr<MixCollection<PSimHit> > col(
+					       new MixCollection<PSimHit> (cf.product(), std::pair<int, int>(-0, 0)));
+    std::cout << *(col.get()) << std::endl;
+    MixCollection<PSimHit>::iterator cfi;
+    int count = 0;
+    for (cfi = col->begin(); cfi != col->end(); cfi++) {
+      std::cout << " Hit " << count << " has tof " << cfi->timeOfFlight() << " trackid "
+		<< cfi->trackId() << " bunchcr " << cfi.bunch() << " trigger " << cfi.getTrigger()
+		<< ", from EncodedEventId: " << cfi->eventId().bunchCrossing() << " "
+		<< cfi->eventId().event() << " bcr from MixCol " << cfi.bunch() << std::endl;
+      std::cout << " Hit: " << (*cfi) << "  " << cfi->exitPoint()<<std::endl;
+      count++;
+    }
+  }
 
 
-	for (simhit_map_iterator it = SimHitMap.begin(); it != SimHitMap.end(); ++it) { // secondo me qua il loop viene fatto sui rivelatori
+  std::auto_ptr<MixCollection<PSimHit> > allRPixHits(
+						     new MixCollection<PSimHit> (cf.product(), std::pair<int, int>(0, 0)));
 
-		edm::DetSet<CTPPSPixelDigi> digi_collector(it->first);
+  if (verbosity_)
+    std::cout << "Input MixCollection size = " << allRPixHits->size() << std::endl;
 
-		if (theAlgoMap.find(it->first) == theAlgoMap.end()) {
-			theAlgoMap[it->first] = boost::shared_ptr<RPixDetDigitizer>(
-			        new RPixDetDigitizer(conf_, *rndEngine, it->first, iSetup)); //per ogni rivelatore creo un digitizer
-		}
+//Loop on PSimHit
+  SimHitMap.clear();
 
-		std::vector<int> input_links;
-		std::vector<std::vector<std::pair<int, double> > >  output_digi_links;   // i links sono i links ai simhits che generano il digi
+  MixCollection<PSimHit>::iterator isim;
+  for (isim = allRPixHits->begin(); isim != allRPixHits->end(); ++isim) {
+    SimHitMap[(*isim).detUnitId()].push_back((*isim));
+  }
 
-
-		(theAlgoMap.find(it->first)->second)->run(SimHitMap[it->first], input_links, digi_collector.data,  output_digi_links);  // runno il digitizer
-
-		std::vector<CTPPSPixelDigi>::iterator pixelIterator = digi_collector.data.begin();
-
-
-		/**
-		 * Exclude all dead channels from vector of strips.
-		 */
-
-//		while(stripIterator != digi_collector.data.end()){
-			//check if channel is dead
-//			if (deadChannelsManager.isChannelDead(stripIterator->GetDetId(), stripIterator->GetStripNo())) {
-				//remove channel from vector
-//				stripIterator = digi_collector.data.erase(stripIterator);
-//			}
-//			else{
-//				stripIterator++;
-//			}
-//		}
-
-		if (digi_collector.data.size() > 0) {
-		  theDigiVector.push_back(digi_collector);   // colleziono i digi di tutti i rivelatori
-		}
-
-	}
-//----------------
-
-	std::unique_ptr<edm::DetSetVector<CTPPSPixelDigi> > digi_output(
-								      new edm::DetSetVector<CTPPSPixelDigi>(theDigiVector));    
-
-	if (verbosity_) {
-		std::cout << "digi_output->size()=" << digi_output->size() << std::endl;
-
-	}
-	// Step D: write output to file
-
-	iEvent.put(std::move(digi_output));
+// Step B: LOOP on hits in event
+  theDigiVector.reserve(400);
+  theDigiVector.clear();
 
 
-//----------------------
-	// Step C: create empty output collection
-/*	std::auto_ptr<edm::DetSetVector<CTPPSPixelDigi> > digi_output(
-								      new edm::DetSetVector<CTPPSPixelDigi>(theDigiVector));    
+  for (simhit_map_iterator it = SimHitMap.begin(); it != SimHitMap.end(); ++it) { 
 
-	if (verbosity_) {
-		std::cout << "digi_output->size()=" << digi_output->size() << std::endl;
+    edm::DetSet<CTPPSPixelDigi> digi_collector(it->first);
 
-	}
-*/	// Step D: write output to file
+    if (theAlgoMap.find(it->first) == theAlgoMap.end()) {
+      theAlgoMap[it->first] = boost::shared_ptr<RPixDetDigitizer>(
+								  new RPixDetDigitizer(conf_, *rndEngine, it->first, iSetup)); //a digitizer for eny detector
+    }
 
-//	iEvent.put(std::make_unique<edm::DetSetVector<CTPPSPixelDigi>>(theDigiVector),"RPixDetDigitizer");
+    std::vector<int> input_links;
+    std::vector<std::vector<std::pair<int, double> > >  output_digi_links;   // links to simhits
+
+
+    (theAlgoMap.find(it->first)->second)->run(SimHitMap[it->first], input_links, digi_collector.data,  output_digi_links);  
+
+    std::vector<CTPPSPixelDigi>::iterator pixelIterator = digi_collector.data.begin();
+
+
+    if (digi_collector.data.size() > 0) {
+      theDigiVector.push_back(digi_collector);   
+    }
+
+  }
+
+
+  std::unique_ptr<edm::DetSetVector<CTPPSPixelDigi> > digi_output(
+								  new edm::DetSetVector<CTPPSPixelDigi>(theDigiVector));    
+
+  if (verbosity_) {
+    std::cout << "digi_output->size()=" << digi_output->size() << std::endl;
+
+  }
+
+  iEvent.put(std::move(digi_output));
 
 }
 
-// ------------ method called once each job just before starting event loop  ------------
-void CTPPSPixelDigiProducer::beginRun(edm::Run&, edm::EventSetup const& es){
-	// get analysis mask to mask channels
-//	if (simulateDeadChannels) {
-//		edm::ESHandle<AnalysisMask> analysisMask;
-//		es.get<TotemDAQMappingRecord> ().get(analysisMask);
-//		deadChannelsManager = DeadChannelsManager(analysisMask); //set analysisMask in deadChannelsManager
-//	}
-}
 
-// ------------ method called once each job just after ending the event loop  ------------
-void CTPPSPixelDigiProducer::endJob() {
-}
+void CTPPSPixelDigiProducer::beginRun(edm::Run&, edm::EventSetup const& es){}
+
+
+void CTPPSPixelDigiProducer::endJob() {}
 
 DEFINE_FWK_MODULE( CTPPSPixelDigiProducer);
--- SimCTPPS/CTPPSPixelDigiProducer/src/RPixChargeShare.cc
+++ SimCTPPS/CTPPSPixelDigiProducer/src/RPixChargeShare.cc
@@ -4,7 +4,7 @@
 #include "TH2D.h"
 
 RPixChargeShare::RPixChargeShare(const edm::ParameterSet &params, uint32_t det_id)
- : det_id_(det_id), theRPixDetTopology_(), sqrt_2(sqrt(2.0))
+  : det_id_(det_id), theRPixDetTopology_(), sqrt_2(sqrt(2.0))
 {
   verbosity_ = params.getParameter<int>("RPixVerbosity");
   signalCoupling_.clear();
@@ -15,12 +15,12 @@ RPixChargeShare::RPixChargeShare(const edm::ParameterSet &params, uint32_t det_i
   signalCoupling_.push_back( (1.0-coupling_constant_)/2 );
   
   no_of_pixels_ = theRPixDetTopology_.detPixelNo();
- fChargeMap = new TFile(ChargeMapFile_.c_str());
- hChargeMap = (TH2D*)fChargeMap->Get("gChargeMap_Angle_0");
+  fChargeMap = new TFile(ChargeMapFile_.c_str());
+  hChargeMap = (TH2D*)fChargeMap->Get("gChargeMap_Angle_0");
 }
 
 std::map<unsigned short, double, std::less<unsigned short> >  RPixChargeShare::Share(
-    const std::vector<RPixSignalPoint> &charge_map)
+										     const std::vector<RPixSignalPoint> &charge_map)
 {
   thePixelChargeMap.clear();
   if(verbosity_>1)
@@ -30,101 +30,101 @@ std::map<unsigned short, double, std::less<unsigned short> >  RPixChargeShare::S
 
   for(std::vector<RPixSignalPoint>::const_iterator i=charge_map.begin(); 
       i!=charge_map.end(); ++i)
-  {
-    double hit_pos_x,hit_pos_y;
+    {
+      double hit_pos_x,hit_pos_y;
 // Used to avoid the abort due to hits out of detector 
 
-    if (((*i).X()+16.6/2)<0||((*i).X()+16.6/2)>16.6) {
-      std::cout << "**** Attention ((*i).X()+simX_width_/2.)<0||((*i).X()+simX_width_/2.)>simX_width  " << std::endl;
-      std::cout << "(*i).X() = " << (*i).X() << std::endl;
-      continue;
-    }
-    if (((*i).Y()+24.4/2.)<0||((*i).Y()+24.4/2.)>24.4) {
-      std::cout << "**** Attention ((*i).Y()+simY_width_/2.)<0||((*i).Y()+simY_width_/2.)>simY_width  " << std::endl;
-      std::cout << "(*i).Y() = " << (*i).Y() << std::endl;
-      continue;
-    }
+      if (((*i).X()+16.6/2)<0||((*i).X()+16.6/2)>16.6) {
+	std::cout << "**** Attention ((*i).X()+simX_width_/2.)<0||((*i).X()+simX_width_/2.)>simX_width  " << std::endl;
+	std::cout << "(*i).X() = " << (*i).X() << std::endl;
+	continue;
+      }
+      if (((*i).Y()+24.4/2.)<0||((*i).Y()+24.4/2.)>24.4) {
+	std::cout << "**** Attention ((*i).Y()+simY_width_/2.)<0||((*i).Y()+simY_width_/2.)>simY_width  " << std::endl;
+	std::cout << "(*i).Y() = " << (*i).Y() << std::endl;
+	continue;
+      }
 
-    std::vector<pixel_info> relevant_pixels = theRPixDetTopology_.getPixelsInvolved(
-    	(*i).X(), (*i).Y(), (*i).Sigma(), hit_pos_x, hit_pos_y);
-    if(verbosity_>1)
-    {
-      std::cout<<"RPixChargeShare "<<det_id_<<" : relevant_pixels = "
-        <<relevant_pixels.size()<<std::endl;
-    }
-    for(std::vector<pixel_info>::const_iterator j = relevant_pixels.begin(); 
-        j!=relevant_pixels.end(); ++j)
-    {
-
-      double effic = (*j).effFactor();
- 
-      unsigned short pixel_no = (*j).pixelIndex();
-      
-      double charge_in_pixel =  (*i).Charge()*effic;
-      
-      CH += charge_in_pixel;
-
+      std::vector<pixel_info> relevant_pixels = theRPixDetTopology_.getPixelsInvolved(
+										      (*i).X(), (*i).Y(), (*i).Sigma(), hit_pos_x, hit_pos_y);
       if(verbosity_>1)
-        std::cout<<"Efficiency in detector "<<det_id_<< " and pixel no " << pixel_no << "  : " <<effic<< "  ch: "<< charge_in_pixel << "   CHtot: "<< CH << std::endl;
+	{
+	  std::cout<<"RPixChargeShare "<<det_id_<<" : relevant_pixels = "
+		   <<relevant_pixels.size()<<std::endl;
+	}
+      for(std::vector<pixel_info>::const_iterator j = relevant_pixels.begin(); 
+	  j!=relevant_pixels.end(); ++j)
+	{
+
+	  double effic = (*j).effFactor();
+ 
+	  unsigned short pixel_no = (*j).pixelIndex();
+      
+	  double charge_in_pixel =  (*i).Charge()*effic;
+      
+	  CH += charge_in_pixel;
+
+	  if(verbosity_>1)
+	    std::cout<<"Efficiency in detector "<<det_id_<< " and pixel no " << pixel_no << "  : " <<effic<< "  ch: "<< charge_in_pixel << "   CHtot: "<< CH << std::endl;
 
 //        QUI SI POTREBBE INTRODURRE IL CHARGE SHARING TRA I PIXELS ..................................       
-      if (signalCoupling_[0]==0.){
-          thePixelChargeMap[pixel_no] += charge_in_pixel;
-      } else {
-       int pixel_row = (*j).pixelRowNo();
-       int pixel_col = (*j).pixelColNo();
-       double pixel_lower_x=0;
-       double pixel_lower_y=0;
-       double pixel_upper_x=0;
-       double pixel_upper_y=0;
-       double pixel_center_x=0;
-       double pixel_center_y=0;
+	  if (signalCoupling_[0]==0.){
+	    thePixelChargeMap[pixel_no] += charge_in_pixel;
+	  } else {
+	    int pixel_row = (*j).pixelRowNo();
+	    int pixel_col = (*j).pixelColNo();
+	    double pixel_lower_x=0;
+	    double pixel_lower_y=0;
+	    double pixel_upper_x=0;
+	    double pixel_upper_y=0;
+	    double pixel_center_x=0;
+	    double pixel_center_y=0;
 
-       theRPixDetTopology_.pixelRange(pixel_row,pixel_col,pixel_lower_x,pixel_upper_x,pixel_lower_y,pixel_upper_y);
-       pixel_center_x = pixel_lower_x + (pixel_upper_x - pixel_lower_x)/2.;
-       pixel_center_y = pixel_lower_y + (pixel_upper_y - pixel_lower_y)/2.;
+	    theRPixDetTopology_.pixelRange(pixel_row,pixel_col,pixel_lower_x,pixel_upper_x,pixel_lower_y,pixel_upper_y);
+	    pixel_center_x = pixel_lower_x + (pixel_upper_x - pixel_lower_x)/2.;
+	    pixel_center_y = pixel_lower_y + (pixel_upper_y - pixel_lower_y)/2.;
 
-        double hit2neighbour[8];
-        double collect_prob = hChargeMap->GetBinContent(hChargeMap->FindBin(((*i).Y()-pixel_center_y)*1.e3,((*i).X()-pixel_center_x)*1.e3));
-       thePixelChargeMap[pixel_no] += charge_in_pixel*collect_prob;
-        unsigned short neighbour_no[8];
-        unsigned short m=0;
-        double closer_neighbour=0;
-	unsigned short closer_no=0;
+	    double hit2neighbour[8];
+	    double collect_prob = hChargeMap->GetBinContent(hChargeMap->FindBin(((*i).Y()-pixel_center_y)*1.e3,((*i).X()-pixel_center_x)*1.e3));
+	    thePixelChargeMap[pixel_no] += charge_in_pixel*collect_prob;
+	    unsigned short neighbour_no[8];
+	    unsigned short m=0;
+	    double closer_neighbour=0;
+	    unsigned short closer_no=0;
 //      Considering the 8 neighbours to share charge
-        for (int k = pixel_row - 1; k <= pixel_row + 1; k++){
-          for (int l = pixel_col - 1; l <= pixel_col + 1; l++){
-           if ((k<0)||k>159||l<0||l>155) continue;
-           if ((k==pixel_row) && (l==pixel_col)) continue;
-           double neighbour_pixel_lower_x=0;
-           double neighbour_pixel_lower_y=0;
-           double neighbour_pixel_upper_x=0;
-           double neighbour_pixel_upper_y=0;
-           double neighbour_pixel_center_x=0;
-           double neighbour_pixel_center_y=0;
+	    for (int k = pixel_row - 1; k <= pixel_row + 1; k++){
+	      for (int l = pixel_col - 1; l <= pixel_col + 1; l++){
+		if ((k<0)||k>159||l<0||l>155) continue;
+		if ((k==pixel_row) && (l==pixel_col)) continue;
+		double neighbour_pixel_lower_x=0;
+		double neighbour_pixel_lower_y=0;
+		double neighbour_pixel_upper_x=0;
+		double neighbour_pixel_upper_y=0;
+		double neighbour_pixel_center_x=0;
+		double neighbour_pixel_center_y=0;
 //         Check the hit approach to the neighbours
-           theRPixDetTopology_.pixelRange(k,l,neighbour_pixel_lower_x,neighbour_pixel_upper_x,neighbour_pixel_lower_y,neighbour_pixel_upper_y);
-           neighbour_pixel_center_x = neighbour_pixel_lower_x + 
-                 (neighbour_pixel_upper_x - neighbour_pixel_lower_x)/2.;
-           neighbour_pixel_center_y = neighbour_pixel_lower_y + 
-                 (neighbour_pixel_upper_y - neighbour_pixel_lower_y)/2.;
-           hit2neighbour[m] = 1/sqrt(pow((*i).X()-neighbour_pixel_center_x,2.)+pow((*i).Y()-neighbour_pixel_center_y,2.));
-           neighbour_no[m]=l*160+k;
-           if (hit2neighbour[m]>closer_neighbour){
-		closer_neighbour=hit2neighbour[m];
-		closer_no = neighbour_no[m];
-	   }
-            m++;
-          }
-        }
-          double chargetransfereff = (1-collect_prob)*signalCoupling_[0];
-	  std::cout << "chargetransfereff : " << chargetransfereff << " " << 1-collect_prob << " "<< signalCoupling_[0]<< std::endl;
-          thePixelChargeMap[closer_no] += charge_in_pixel*chargetransfereff;
+		theRPixDetTopology_.pixelRange(k,l,neighbour_pixel_lower_x,neighbour_pixel_upper_x,neighbour_pixel_lower_y,neighbour_pixel_upper_y);
+		neighbour_pixel_center_x = neighbour_pixel_lower_x + 
+		  (neighbour_pixel_upper_x - neighbour_pixel_lower_x)/2.;
+		neighbour_pixel_center_y = neighbour_pixel_lower_y + 
+		  (neighbour_pixel_upper_y - neighbour_pixel_lower_y)/2.;
+		hit2neighbour[m] = 1/sqrt(pow((*i).X()-neighbour_pixel_center_x,2.)+pow((*i).Y()-neighbour_pixel_center_y,2.));
+		neighbour_no[m]=l*160+k;
+		if (hit2neighbour[m]>closer_neighbour){
+		  closer_neighbour=hit2neighbour[m];
+		  closer_no = neighbour_no[m];
+		}
+		m++;
+	      }
+	    }
+	    double chargetransfereff = (1-collect_prob)*signalCoupling_[0];
+	    std::cout << "chargetransfereff : " << chargetransfereff << " " << 1-collect_prob << " "<< signalCoupling_[0]<< std::endl;
+	    thePixelChargeMap[closer_no] += charge_in_pixel*chargetransfereff;
 
 //	  thePixelChargeMap[pixel_no] += charge_in_pixel*(1-chargetransfereff);
-      }
+	  }
+	}
     }
-  }
 
 
   double CHS=0;
@@ -132,6 +132,6 @@ std::map<unsigned short, double, std::less<unsigned short> >  RPixChargeShare::S
     std::cout << "carica nella mappa post sharing  "<<IO.second << std::endl;
     CHS += IO.second;
   }
-   std::cout << "carica TOTALE post sharing  "<<CHS << std::endl;
+  std::cout << "carica TOTALE post sharing  "<<CHS << std::endl;
   return thePixelChargeMap;
 }
